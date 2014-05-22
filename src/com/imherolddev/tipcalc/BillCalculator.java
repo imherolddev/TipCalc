5c1b489db99b8e68993adbc266a196bc018924ed
Jason Hall <imherolddev@gmail.com>
2014-05-21 23:10:52 -0700

    New and improved
    -better currency calculations
    -improved currency display
    -added text views to clear button action

--- src/com/imherolddev/tipcalc/BillCalculator.java
+++ src/com/imherolddev/tipcalc/BillCalculator.java
@@ -1,16 +1,33 @@
 package com.imherolddev.tipcalc;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+
 public class BillCalculator {
 	
-	private double totalBill = 0.00;
-	private int tipTotal = 0;
+	private BigDecimal tipTotal;
+	private BigDecimal billTotal;
+	private final BigDecimal PERCENT = new BigDecimal("100");
+	NumberFormat format = NumberFormat.getCurrencyInstance();
+	BigDecimal tipDisplay;
 	
-	public double calc(double bill, int tipPercent) {
+	
+	public String calcBill(String bill, String tip) {
 		
-		tipTotal = (int) (bill * (double)(tipPercent / 100));
-		totalBill = bill + tipTotal;
+		BigDecimal billAmt = new BigDecimal(bill);
+		BigDecimal tipPercent = new BigDecimal(tip);
+		tipTotal = billAmt.multiply(tipPercent.divide(PERCENT));
 		
-		return totalBill;
+		billTotal = billAmt.add(tipTotal);
+		
+		BigDecimal billDisplay = billTotal.setScale(2, RoundingMode.HALF_EVEN);
+		tipDisplay = tipTotal.setScale(2, RoundingMode.HALF_EVEN);
+		
+		format.setMinimumFractionDigits(1);
+		format.setMaximumFractionDigits(2);
+		
+		return format.format(Double.parseDouble(billDisplay.toString()));
 		
 	}
 	
@@ -19,6 +36,12 @@ public class BillCalculator {
 		int newTotal = 0;
 		
 		return newTotal;
+		
+	}
+	
+	public String getTipTotal() {
+		
+		return format.format(Double.parseDouble(this.tipDisplay.toString()));
 		
 	}
 
--- src/com/imherolddev/tipcalc/MainActivity.java
+++ src/com/imherolddev/tipcalc/MainActivity.java
@@ -13,91 +13,98 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
-	
+
+	private EditText etBill;
+	private EditText etTip;
+	private TextView billTotal;
+	private TextView tipTotal;
+
 	BillCalculator calculator = new BillCalculator();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    
-    public void calcBill(View v) {
-    	
-    	EditText etBill = (EditText) findViewById(R.id.et_billAmt);
-    	EditText etTip = (EditText) findViewById(R.id.et_tipAmt);
-    	TextView tvTotal = (TextView) findViewById(R.id.tv_billTotalMoney);
-    	TextView tvTip = (TextView) findViewById(R.id.tv_tipTotalMoney);
-    	
-    	double total = 0.00;
-    	
-    	if(etBill.getText().toString().equals("") | etBill.getText().toString().equals("0")) {
-    		
-    		Toast.makeText(this, getString(R.string.tst_billInvalid), Toast.LENGTH_LONG).show();
-    		
-    	} else if(etTip.getText().toString().equals("") | etTip.getText().toString().equals("0")) {
-    		
-    		Toast.makeText(this, getString(R.string.tst_tipInvalid), Toast.LENGTH_LONG).show();
-    		
-    	} else {
-    		
-    		total = calculator.calc(Double.parseDouble(etBill.getText().toString()), Integer.parseInt(etTip.getText().toString()));
-    		
-    	}
-    	
-    }
-    
-    public void clearVals(View v) {
-    	
-    	EditText etBill = (EditText) findViewById(R.id.et_billAmt);
-    	EditText etTip = (EditText) findViewById(R.id.et_tipAmt);
-    	
-    	etBill.setText("");
-    	etTip.setText("");
-    	
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+	public void calcBill(View v) {
 
-        public PlaceholderFragment() {
-        }
+		etBill = (EditText) findViewById(R.id.et_billAmt);
+		etTip = (EditText) findViewById(R.id.et_tipAmt);
+		billTotal = (TextView) findViewById(R.id.tv_billTotalMoney);
+		tipTotal = (TextView) findViewById(R.id.tv_tipTotalMoney);
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
+		if (etBill.getText().toString().equals("")
+				| etBill.getText().toString().equals("0")) {
+
+			Toast.makeText(this, getString(R.string.tst_billInvalid),
+					Toast.LENGTH_LONG).show();
+
+		} else if (etTip.getText().toString().equals("")
+				| etTip.getText().toString().equals("0")) {
+
+			Toast.makeText(this, getString(R.string.tst_tipInvalid),
+					Toast.LENGTH_LONG).show();
+
+		} else {
+
+			billTotal.setText(calculator.calcBill(etBill.getText().toString(),
+					etTip.getText().toString()));
+			tipTotal.setText(calculator.getTipTotal());
+
+		}
+
+	}
+
+	public void clearVals(View v) {
+
+		etBill.setText("");
+		etTip.setText("");
+		billTotal.setText(getString(R.string.tv_defaultAmt));
+		tipTotal.setText(getString(R.string.tv_defaultAmt));
+
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
 
 }
